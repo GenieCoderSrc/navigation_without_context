c8667dd7f968e896e574e8211c3860899bb2277f
SuraiyaKhatunItHome <suraiya.it.home@gmail.com>
2025-04-09 08:53:12 +0600

    created example file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,17 @@
-## 0.0.1
+# Changelog
 
-* TODO: Describe initial release.
+All notable changes to this project will be documented in this file.
+
+## 0.0.1
+### Added
+- Initial release of `navigation_wihout_context`.
+- Added global navigator key setup.
+- Support for navigation methods without BuildContext:
+    - `pushNameRoute`
+    - `popRoute`
+    - `pushReplacementRoute`
+    - `pushNamedAndRemoveUntil`
+    - `popAndPushNamed`
+- Exported `AppRouter` class.
+- Included example project demonstrating usage.
+
---
+++ example/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_wihout_context/navigation_wihout_context.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: AppRouter.navigatorKey,
+      title: 'Navigation Without Context Example',
+      routes: {
+        '/': (context) => HomePage(),
+        '/second': (context) => SecondPage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            AppRouter.pushNameRoute('/second', arguments: {'message': 'Hello from Home!'});
+          },
+          child: Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final message = args?['message'] ?? 'No message';
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(message),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                AppRouter.popRoute();
+              },
+              child: Text('Go Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
